3524105ac14845eace3579151655aa736aec00d6
MedZiedJlassi <mzied.jlassi@gmail.com>
2021-07-14 12:42:55 +0100

    push 140721

--- page/module/administrateur/communication/liste_comm.php
+++ page/module/administrateur/communication/liste_comm.php
@@ -184,8 +184,7 @@ if(empty($_SESSION['id'])){
                 </div>
                 <div class="content-header-right text-md-right col-md-3 col-12 d-md-block d-none">
                     <div class="form-group breadcrumb-right">
-                        <a class="btn-icon btn btn-primary btn-round btn-sm waves-effect waves-float waves-light" href="modif_comm.php">Ajouter une communication</a>
-                        
+                        <a class="btn-icon btn btn-primary btn-round btn-sm waves-effect waves-float waves-light" href="modif_comm.php">Ajouter une com</a>
                     </div>
                 </div>
             </div>
